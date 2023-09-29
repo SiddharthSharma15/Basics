4088faccb73ca41a9dadb7835c5adb2276d03292
SiddharthSharma15 <siddharthchd15@gmail.com>
2023-09-29 14:12:57 +0530

    make intentional errors for testing the conflicts

--- ml_model.py
+++ ml_model.py
@@ -6,10 +6,9 @@ def model1():
     print('adding code for ML model1')
 
 def model2():
-    print('adding code for ML model2')
+    print('making a change - adding code for ML model2')
 
 def model3():
     print('adding code for ML model3')
-
 
 print('MOdel2 is performing best')
